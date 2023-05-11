fc656f27658f2353dec07aac5ffe9c77ea7b3052
Edgard Lino da Silva <mosmack@gmail.com>
2023-05-11 18:34:41 -0300

    atualizando codigo

--- aulaJava.java
+++ aulaJava.java
@@ -1,27 +1,14 @@
 import java.util.Scanner;
- 
-public class MyClass {
-    
-    public static int soma (int a, int b) {
-        return a + b;
-    }
- 
+public class Exercicio1 {
     public static void main(String args[]) {
         Scanner sc1 = new Scanner(System.in);
-        
-        soma(1,2);
-        
         int soma = 0;
         
-        
         int contadorDeLimite = 0;
-        
         while(sc1.hasNext() && contadorDeLimite < 2) {
             soma = soma + sc1.nextInt();
             contadorDeLimite++;
         }
-        
-        System.out.println(soma =  + soma);
-        
+        System.out.println(soma =+ soma);
     }
 }

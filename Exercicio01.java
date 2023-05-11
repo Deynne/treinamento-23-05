7051cf31d75fbdf908cb183f6b1f023fbd9d3e82
Alexandre Lima <lima.ale10@gmail.com>
2023-05-11 18:20:45 -0300

    Exercicio JAVA

---
+++ Exercicio01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Exercicio01{
+ 
+    public static void main(String args[]) {
+        Scanner sc1 = new Scanner(System.in);
+        int soma = 0;
+        int contadorDeLimite = 0;
+        while(sc1.hasNext() && contadorDeLimite < 2) {
+            soma = soma + sc1.nextInt();
+            contadorDeLimite++;
+        }
+        System.out.println(soma =+ soma);     
+    }
+}
+
+

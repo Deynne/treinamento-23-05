31d8418a5fff8acc9091724b0aeddd2e3beb2336
Carlos Cubas <carloscubas1@yahoo.com.br>
2023-05-15 17:57:50 -0300

    exercicio1

---
+++ exercicios/exercicio1.java
@@ -0,0 +1,12 @@
+package Exercicio.com.treinamnto;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+			for(int contador = 0; contador <= 1000; contador++) {
+		    	System.out.println(contador);
+			}
+
+		}
+
+	}

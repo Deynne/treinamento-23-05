9f22dbabbb2f7e039a9dc9f121e37e65306aacc5
Carlos Cubas <carloscubas1@yahoo.com.br>
2023-05-15 22:09:30 -0300

    exercicio3

--- exercicios/exercicio3.java
+++ exercicios/exercicio3.java
@@ -3,13 +3,13 @@ package com.treinamento.exercicio;
 public class exercicio3 {
 
 	public static void main(String[] args) {
-        int nA = 73;
+        int nA = 5;
         
-        if(nA % 7 == 0 ||nA % 5 == 0 || nA % 3 == 0 ||  nA % 2 == 0){
-            System.out.println("é primo");
+        if(nA % 7 == 0 && nA != 7 ||nA % 5 == 0 && nA != 5 || nA % 3 == 0 && nA != 3 ||  nA % 2 == 0 && nA != 2){
+            System.out.println("não é primo");
             }else
             {
-                System.out.println("não é primo");
+                System.out.println("é primo");
             }
     }
 }

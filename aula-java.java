53abefea7f1dbb9c0c0e4f52e328936ef642f5f1
eliane <mitecnol2023@gmail.com>
2023-06-08 18:50:54 -0300

    adicionando arquivo aula java

---
+++ aula-java.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MyClass{
+    
+    public static int soma (int a, int b){
+        return a + b;
+}
+
+    public static void main(String args[]){
+        Scanner sc1 = new Scanner(System.in);
+        
+        soma(1,2);
+        
+        int soma = 0;
+        
+        
+        int contadorDeLimite = 0;
+        
+        while(sc1.hasNext() && contadorDeLimite < 2){
+            soma = soma + sc1.nextInt();
+            contadorDeLimite++;
+        }
+        
+        System.out.println("soma = " + soma);
+        
+    }
+}
